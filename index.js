bed985e60d793fb70e7a3649e1d54fd26cba6db1
thecuriousones <tamaraleonard46@gmail.com>
2021-03-15 00:24:29 -0400

    done

--- index.js
+++ index.js
@@ -129,8 +129,11 @@ Use the higher order function getAverageGoals to do the following:
  Example of invocation: getAverageGoals(getFinals(fifaData));
 */
 
-function getAverageGoals(/* code here */) {
-   /* code here */
+function getAverageGoals(arrayCallBack) {
+    let totalNumber = arrayCallBack.reduce((accumulator, data) => {
+        return accumulator + data["Home Team Goals"] + data["Away Team Goals"];
+    },0)
+    return (totalNumber/ arrayCallBack.length).toFixed(2);
 }
 
 
